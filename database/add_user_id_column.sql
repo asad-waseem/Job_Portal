2c4a8b792c500c14198d54974795ef76443c6eee
asad-waseem <asadwaseem.tech@gmail.com>
2026-02-06 19:37:39 +0500

    Cleanup: Consolidate database, rename index file, remove unused scripts

--- database/add_user_id_column.sql
+++
@@ -1,6 +0,0 @@
--- Add user_id column to job_applications table
-ALTER TABLE job_applications ADD COLUMN user_id INT(11) NULL AFTER cover_letter;
-
--- Add foreign key constraint (optional but recommended)
--- ALTER TABLE job_applications ADD CONSTRAINT fk_applications_user 
--- FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE SET NULL;
--- database/clear_users.sql
+++
@@ -1,5 +0,0 @@
--- Delete all users from database
-DELETE FROM users;
-
--- Reset auto-increment for users table
-ALTER TABLE users AUTO_INCREMENT = 1;
